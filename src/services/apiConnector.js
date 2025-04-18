bee0d9dff3752b265d985b00050893cc9bd871b6
anshika-maurya <anshikamaurya6400@gmail.com>
2025-04-18 13:56:48 +0530

    commits fixed

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -1,8 +1,19 @@
 import axios from "axios";
-export const axiosInstance = axios.create({});
 
+// Directly use the local URL for development
+const BASE_URL = "http://localhost:4000/api/v1";
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 15000,
+  headers: {
+    "Content-Type": "application/json",
+  }
+});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
+  console.log("API Call:", method, url);
+  
   return axiosInstance({
     method: `${method}`,
     url: `${url}`,
@@ -10,5 +21,4 @@ export const apiConnector = (method, url, bodyData, headers, params) => {
     headers: headers ? headers : null,
     params: params ? params : null,
   }); 
-  
 }
